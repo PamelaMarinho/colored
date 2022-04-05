493cba220af548434d497922f0b2c82d07474bdb
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-04-05 17:12:09 -0300

    update

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+let listColor = []
+
 function colorBox(){
         const box = document.getElementsByClassName('boxColor')
         box[0].setAttribute('class','boxColor-display')
 }
 
-
-
-function getColor(){
-        const inputColor = document.getElementById('cor').value 
-        document.getElementById('put').value = inputColor
-
-
+function getId(id){
+    getColor(id)
 }
+
+function getColor(id){
+        const inputColor = document.getElementById(`cor${id}`).value 
+        console.log(inputColor)
+        document.getElementById(`put${id}`).value = inputColor
+        listColor.push(inputColor)
+        
+        changeBg()
+}
+
+function changeBg(){
+    const colors = document.getElementsByClassName('boxColor-display')
+    const bg = document.getElementById('background')
+    console.log([...listColor])
+    bg.style.background= `${'linear'}-gradient(to right,${[...listColor]})`
+}
+
+function a(){
+    console.log(listColor)
+}
+
+a()

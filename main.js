1d793fcbeab22a09bc11a35e81890f06b0ca19c3
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-04-05 20:14:58 -0300

    update

--- main.js
+++ main.js
@@ -9,19 +9,22 @@ function getId(id){
     getColor(id)
 }
 
+function changeColor(color){
+    alert('jhkj')
+    console.log('cor',color)
+}
+
 function getColor(id){
         const inputColor = document.getElementById(`cor${id}`).value 
-        console.log(inputColor)
         document.getElementById(`put${id}`).value = inputColor
         listColor.push(inputColor)
-        
         changeBg()
 }
 
 function changeBg(){
-    const colors = document.getElementsByClassName('boxColor-display')
+    const colors = docum.getElementsByClassName('boxColor-display')
     const bg = document.getElementById('background')
-    console.log([...listColor])
+    //console.log([...listColor])
     bg.style.background= `${'linear'}-gradient(to right,${[...listColor]})`
 }
 

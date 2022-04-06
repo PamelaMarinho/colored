b4ba7d61afdd15c342418e798d71d44724c31edc
pamelamarinho <pamelamarinhopm@hotmail.com>
2022-04-06 10:27:05 -0300

    update bg

--- main.js
+++ main.js
@@ -22,29 +22,44 @@ function getId(id){
     getColor(id)
 }
 
-function changeColor(color){
-    alert('jhkj')
-    console.log('cor',color)
+function getColor(id){
+    
+    const inputColor = document.getElementById(`cor${id}`).value 
+
+    console.log('antes',listColor)
+    
+     listColor.splice(id-1,1,inputColor)
+    //console.log(id-1,inputColor)
+
+    document.getElementById(`put${id}`).value = inputColor
+    
+    changeBg()
+    
+    //console.log('depois',listColor)
+
+/*     const aj =  [1,2,3,6,8,5,7]
+
+aj.splice(id-1,1,inputColor)
+
+console.log(aj) */
+     
 }
 
-function getColor(id){
-        const inputColor = document.getElementById(`cor${id}`).value 
-        document.getElementById(`put${id}`).value = inputColor
-        listColor.push(inputColor)
-        changeBg()
-}
 
 /************ altera background ******************/
 
 function changeBg(){
-    const colors = docum.getElementsByClassName('boxColor-display')
+
+    console.log('depois',listColor)
+
+    const colors = document.getElementsByClassName('boxColor-display')
     const bg = document.getElementById('background')
-    //console.log([...listColor])
+   // console.log([...listColor])
     bg.style.background= `${'linear'}-gradient(to right,${[...listColor]})`
 }
 
 function a(){
-    console.log(listColor)
+  //  console.log(listColor)
 }
 
 a()
@@ -53,9 +68,6 @@ function closeBox(){
     const box = document.getElementsByClassName('boxColor-display')
         box[0].setAttribute('class','boxColor')
 }
-
-
-
 
 
 
